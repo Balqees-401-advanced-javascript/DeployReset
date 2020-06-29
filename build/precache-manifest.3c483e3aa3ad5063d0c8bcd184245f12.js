1258fce839f4ff9caa8d3a929eb57e693be7d513
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-06-29 14:41:21 +0300

    test

---
+++ build/precache-manifest.3c483e3aa3ad5063d0c8bcd184245f12.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efcb08d0bcb28e44da0a44daf0b8a5c",
+    "url": "/Balqees-401-advanced-javascript/DeployReset/index.html"
+  },
+  {
+    "revision": "c1f222a5177c3c073ebb",
+    "url": "/Balqees-401-advanced-javascript/DeployReset/static/js/2.68acf433.chunk.js"
+  },
+  {
+    "revision": "bcbe1977553d1b9985cf",
+    "url": "/Balqees-401-advanced-javascript/DeployReset/static/js/main.814dc061.chunk.js"
+  },
+  {
+    "revision": "c3dfc550493173a57fcf",
+    "url": "/Balqees-401-advanced-javascript/DeployReset/static/js/runtime~main.f5753474.js"
+  }
+]);
