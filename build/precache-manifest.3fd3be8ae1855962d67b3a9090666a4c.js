af0cf0946206b292b4a6110a3ea8eacfd95e4b3a
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-06-29 16:24:54 +0300

    test

---
+++ build/precache-manifest.3fd3be8ae1855962d67b3a9090666a4c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7168f3e83986a5cce14236032f1aaa",
+    "url": "/DeployReset/index.html"
+  },
+  {
+    "revision": "c1f222a5177c3c073ebb",
+    "url": "/DeployReset/static/js/2.68acf433.chunk.js"
+  },
+  {
+    "revision": "bcbe1977553d1b9985cf",
+    "url": "/DeployReset/static/js/main.814dc061.chunk.js"
+  },
+  {
+    "revision": "33423a0d7dbd00d803aa",
+    "url": "/DeployReset/static/js/runtime~main.3d5406d4.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import App from './app.js';
 class Main extends React.Component {
   render() {
     return <App />;
-    
   }
 }
 
